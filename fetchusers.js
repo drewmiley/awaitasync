163639e61e94f21d66f90c92c425dc65ae5bb2e5
Drew Miley <drew.miley@Drews-MacBook-Pro.local>
2018-10-29 16:12:27 +0000

    Adds fetch users example

---
+++ fetchusers.js
@@ -0,0 +1,12 @@
+var fetch = require("node-fetch");
+
+async function fetchUsers(endpoint) {
+    const res = await fetch(endpoint);
+    let data = await res.json();
+  
+    data = data.map(user => user.username);
+  
+    console.log(data);
+  }
+  
+  fetchUsers('https://jsonplaceholder.typicode.com/users');
