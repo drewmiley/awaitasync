25d31d0e562fb79052cb2bdace2a63d485e7e16f
Drew Miley <drew.miley@Drews-MacBook-Pro.local>
2018-10-30 09:21:32 +0000

    Adds user data chaining for await async

--- userdatacomparison.js
+++ userdatacomparison.js
@@ -6,7 +6,21 @@ const getFirstUserDataPromises = (url) => {
     .then(users => users[0]) // pick first user
     .then(user => fetch(`${url}/${user.id}`)) // get user data
     .then(userResponse => userResponse.json())
-    .then(user => console.log(user))
 }
 
-getFirstUserDataPromises('https://jsonplaceholder.typicode.com/users')
+const userDataPromises = getFirstUserDataPromises('https://jsonplaceholder.typicode.com/users')
+
+userDataPromises.then(console.log)
+
+const getFirstUserDataAwaitAsync = async (url) => {
+  const response = await fetch(url) // get users list
+  const users = await response.json() // parse JSON
+  const user = users[0] // pick first user
+  const userResponse = await fetch(`${url}/${user.id}`) // get user data
+  const userData = await userResponse.json()
+  return userData
+}
+
+const userDataAwaitAsync = getFirstUserDataAwaitAsync('https://jsonplaceholder.typicode.com/users')
+
+userDataAwaitAsync.then(console.log)
