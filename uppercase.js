ebdab0594c0383f2fb002b16eeacd3bddc7cd7b4
Drew Miley <drew.miley@Drews-MacBook-Pro.local>
2018-10-30 09:50:57 +0000

    ES6 and standard uppercase

--- uppercase.js
+++ uppercase.js
@@ -1,17 +1,20 @@
-function caserUpper(val) {
+const caserUpper = val => {
   return new Promise((resolve, reject) => {
-    resolve(val.toUpperCase());
-  });
+    resolve(val.toUpperCase())
+  })
 }
-  
-async function msg(x) {
+
+const msg = async x => {
   try {
-    const msg = await caserUpper(x);
-    console.log(msg);
-  } catch(err) {
-    console.log('Ohh no:', err.message);
+    const msg = await caserUpper(x)
+    console.log(msg)
+  } catch (err) {
+    console.log('Ohh no:', err.message)
   }
 }
 
-msg('Hello'); // HELLO
-msg(34); // Ohh no: val.toUpperCase is not a function
+// HELLO
+msg('Hello')
+
+// Ohh no: val.toUpperCase is not a function
+msg(34)
